25f670302895751af55502de2b27843f27c126de
Michaela Budde <michaelabudde@Michaelas-MacBook-Pro.local>
2023-05-26 20:53:53 -0700

    part 1

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Guanajuato, MX",
+    link: "https://unsplash.com/photos/H4VrfB98ddA",
+  },
+  {
+    name: "Rio de Janiero, BR",
+    link: "https://unsplash.com/photos/7F65HDP0-E0",
+  },
+  {
+    name: "San Juan, PR",
+    link: "https://unsplash.com/photos/48iZAbNgU8U",
+  },
+  {
+    name: "Querétaro, MX",
+    link: "https://unsplash.com/photos/RZ_EIT0IXbg",
+  },
+  {
+    name: "Puerto Vallarta, MX",
+    link: "https://unsplash.com/photos/f60ysZEQgR8",
+  },
+  {
+    name: "Barcelona, ES",
+    link: "https://unsplash.com/photos/N6HTCyN50p0",
+  },
+];
